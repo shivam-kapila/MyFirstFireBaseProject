7daaa91c63f409202da5b93f2edb9a1747ceede4
shivam-kapila <shivamkapila4@gmail.com>
2019-02-18 02:04:55 +0530

    Initial commit

---
+++ app/src/main/java/com/shivamkapila/myfirstfirebaseproject/ProfileActivity.java
@@ -0,0 +1,31 @@
+package com.shivamkapila.myfirstfirebaseproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.d("MainActivity", "seee");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+
+        findViewById(R.id.buttonLogout).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+
+                Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
